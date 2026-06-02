449393c0fad156cc45d47bad06023f527c204f92
gelsonh <gelsonpf49@hotmail.com>
2026-06-02 15:48:57 -0400

    responsive the-code view

--- services/fizzbuzz_service.py
+++ services/fizzbuzz_service.py
@@ -4,14 +4,12 @@
 # Inicio
 def generate_fizzbuzz(fizz_value, buzz_value, stop_value):
     results = []
-
     #     Para Numero ← 1 Hasta StopValue Hacer
     for n in range(1, stop_value + 1):
         #         Si Numero es divisible por FizzValue y BuzzValue Entonces
         #             Mostrar "FizzBuzz"
         if n % fizz_value == 0 and n % buzz_value == 0:
             results.append("FizzBuzz")
-
         #         Sino Si Numero es divisible por FizzValue Entonces
         #             Mostrar "Fizz"
         elif n % fizz_value == 0:
@@ -24,7 +22,6 @@ def generate_fizzbuzz(fizz_value, buzz_value, stop_value):
         #             Mostrar Numero
         else:
             results.append(n)
-
         #         Fin Si
     return results
 
